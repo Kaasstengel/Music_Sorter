fe6e37d069842320d1a03bb6f616fb5c1838c416
kaasstengel <casdemooij@gmail.com>
2019-01-02 13:20:29 +0100

    Working on a file browser and added a requirements file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Music_Sorter
-A piece of software with wich you can sort a big amount of music in a quick and efficient way. This project is a hobby project for me and my DJ buddies.
+A piece of software with which you can sort a big amount
+ of music in a quick and efficient way. 
+ 
+ This project is a hobby project for me and my DJ buddies.
--- music_sorter/application.py
+++ music_sorter/application.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import filedialog
+from tkinter import ttk
 
 
 class Application(tk.Frame):
@@ -6,22 +8,37 @@ class Application(tk.Frame):
         super().__init__(master)
         self.master = master
         self.pack()
+        self.create_menu()
         self.create_widgets()
 
+    def create_menu(self):
+        self.menubar = tk.Menu(root)
+        root.config(menu=self.menubar)
+
+        self.subMenu = tk.Menu(self.menubar, tearoff=0)
+
+        self.menubar.add_cascade(label="File", menu=self.subMenu)
+        self.subMenu.add_command(label="Open", command=self.browse_file)
+        self.subMenu.add_command(label="Exit", command=root.destroy)
+
     def create_widgets(self):
-        self.test = tk.Button(self)
-        self.test["text"] = "testy testy"
-        self.test["command"] = self.console_warning
-        self.test.pack(side="top")
+        self.browse_file_click = tk.Button()
+        self.browse_file_click["text"] = "Click me to browse files!"
+        self.browse_file_click.pack(side="top")
+        self.browse_file_click["command"] = self.browse_file
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def console_warning(self):
-        print("weeeooooweeeoooo")
+    def browse_file(self):
+        global filenames
+        filenames = filedialog.askopenfilenames(parent=root, title='choose a file')
+        print(root.tk.splitlist(filenames))
 
 
 root = tk.Tk()
 app = Application(master=root)
+
+statusbar = ttk.Label(root, text="Please choose the files you want sorted")
+statusbar.pack(side="bottom", fill="x")
+
+
+
 app.mainloop()
